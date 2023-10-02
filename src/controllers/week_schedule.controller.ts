ced3631497f078165a448a07e3eeb02c3702b0c5
ltlaitoff <ltlaitoff@gmail.com>
2023-10-02 09:40:39 +0300

    feat: Added to dates date as string of start and end

--- src/controllers/week_schedule.controller.ts
+++ src/controllers/week_schedule.controller.ts
@@ -3,9 +3,24 @@ import { getLessonTypeText } from '../helpers/get_lesson_type_text.helper'
 import { getPairsByWeekDay } from '../helpers/get_pairs_by_week_day.helper'
 
 export function weekScheduleController(next = false) {
+	const startWeekDate = new Date()
+	const daytoset = next ? 8 : 1
+
+	const currentDay = startWeekDate.getDay()
+	const distance = daytoset - currentDay
+	startWeekDate.setDate(startWeekDate.getDate() + distance)
+
+	const endWeekDate = new Date()
+	endWeekDate.setDate(startWeekDate.getDate() + 6)
+
+	const startWeekDateString = startWeekDate.toLocaleDateString('en-GB')
+	const endWeekDateString = endWeekDate.toLocaleDateString('en-GB')
+
 	const weekText = `Розклад на ${
 		next ? 'наступний' : 'цей'
-	} тиждень(${getLessonTypeText(next)}):\n\n`
+	} тиждень(${startWeekDateString} - ${endWeekDateString}) (${getLessonTypeText(
+		next
+	)}):\n\n`
 
 	let scheduleText = ''
 
